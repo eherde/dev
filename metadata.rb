279ab7eca4549a195139dd79ee25a0dfd3a834a9
Eric Herde <eherde@users.noreply.github.com>
2015-04-24 12:04:07 -0700

    Initial commit
    * generate dev cookbook

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'dev'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures dev'
+long_description 'Installs/Configures dev'
+version          '0.1.0'
+
