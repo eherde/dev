ae5da3a620a8e147541030700f2cc581c1ec4f71
Eric Herde <eherde@users.noreply.github.com>
2015-04-24 15:32:09 -0700

    implement zsh recipe

--- recipes/zsh.rb
+++ recipes/zsh.rb
@@ -3,3 +3,27 @@
 # Recipe:: zsh
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+header = "### Generate by Chef. DO NOT EDIT! ###"
+
+home = node['etc']['passwd'][node[:user]]['dir']
+
+template "#{home}/.zshrc" do
+  owner node[:user]
+  group node[:user]
+  mode '0600'
+  source 'zshrc.erb'
+  variables({
+    :header => header
+  })
+end
+
+template "#{home}/.aliases" do
+  owner node[:user]
+  group node[:user]
+  mode '0600'
+  source 'aliases.erb'
+  variables({
+    :header => header
+  })
+end
