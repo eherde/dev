5a91f65b4217604cdfe6da0d6f0fe17146509ec6
Eric Herde <eherde@users.noreply.github.com>
2015-04-27 16:57:49 -0700

    implement tmux recipe

--- recipes/tmux.rb
+++ recipes/tmux.rb
@@ -3,3 +3,17 @@
 # Recipe:: tmux
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+header = "### Generate by Chef. DO NOT EDIT! ###"
+
+home = node['etc']['passwd'][node[:user]]['dir']
+
+template "#{home}/.tmux.conf" do
+  owner node[:user]
+  group node[:user]
+  mode '0600'
+  source 'tmux.conf.erb'
+  variables({
+    :header => header
+  })
+end
