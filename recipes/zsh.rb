f3d17aadbe97922baeafecf768b18cd7163344a9
Eric Herde <eherde@users.noreply.github.com>
2016-03-02 15:02:54 -0800

    use zshenv
    Use zshenv, which is sourced first

--- recipes/zsh.rb
+++ recipes/zsh.rb
@@ -6,6 +6,14 @@
 
 header = "### Generate by Chef. DO NOT EDIT! ###"
 
+template "#{node.dev.home}/.zshenv" do
+  mode '0600'
+  source 'zshenv.erb'
+  variables({
+    :header => header
+  })
+end
+
 template "#{node.dev.home}/.zshrc" do
   mode '0600'
   source 'zshrc.erb'
@@ -17,14 +25,6 @@ end
 template "#{node.dev.home}/.aliases" do
   mode '0600'
   source 'aliases.erb'
-  variables({
-    :header => header
-  })
-end
-
-template "#{node.dev.home}/.envrc" do
-  mode '0600'
-  source 'envrc.erb'
   variables({
     :header => header
   })
